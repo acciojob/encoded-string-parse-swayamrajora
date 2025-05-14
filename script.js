c54528c667045f5abc4022fe7c1a3d365ea03e50
swayamrajora <127737419+swayamrajora@users.noreply.github.com>
2025-05-14 08:13:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const parseCode = (str) => {
-  // your code here
-};
+function parseCode(str) {
+  // Split the string by '0' and filter out empty strings
+  const parts = str.split('0').filter(part => part !== '');
+  
+  return {
+    firstName: parts[0],
+    lastName: parts[1],
+    id: parts[2]
+  };
+}
 
 // Do not change the code below
 const str = prompt("Enter str: ");
 alert(JSON.stringify(parseCode(str)));
+ encodedstring prase
